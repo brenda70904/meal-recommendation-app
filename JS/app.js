4a94a09796fbd562c2d6a7effb15cbd990ad31fa
Jason Christopher <jchristopher2448@gmail.com>
2022-10-24 18:15:07 -0500

    pushing so I can pull

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,17 @@
 'use strict'
+
+let results = [];
+
+for (let i = 0; i < allRecipes.length; i++) {
+  let match = true;
+  for (let j = 0; j < allRecipes.ingredients.length; j++) {
+    if(myIngredients.includes(allRecipes[i].ingredients[j])) {
+    } else {
+      match = false;
+      break;
+    }
+    if (match === true) {
+      results.push(allRecipes[i]);
+    }
+  }
+}
