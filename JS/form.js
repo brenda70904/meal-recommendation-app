149ac3b7275968b77b1636bb5ea95abb70f84a8f
Jason Christopher <jchristopher2448@gmail.com>
2022-10-28 09:41:43 -0500

    got select all to work

--- JS/form.js
+++ JS/form.js
@@ -19,12 +19,12 @@ for (let i = 0; i < dropList.length; i++) {
 
 const form = document.querySelector('form');
 
-let allProtein = ['chicken breast', 'chicken thighs', 'pork', 'beef brisket', 'flank steak', 'corned beef', 'ground beef', 'salmon', 'spam', 'egg', 'black beans'].sort();
-let allVegFruit = ['garlic', 'ginger', 'onion', 'green onion', 'tomato', 'potato', 'green beans', 'peppers', 'mushrooms', 'carrot', 'peas', 'cabbage', 'seaweed', 'hominy', 'guajillo chile', 'cilantro', 'basil', 'lemon', 'lime', 'bok choy', 'broccoli', 'edamame', 'corn'].sort();
-let allDairy = ['feta cheese', 'parmesan cheese', 'cheddar cheese', 'cheese blend', 'milk', 'cream', 'butter', 'coconut milk', 'yogurt'].sort();
-let allCarbs = ['ramen noodles', 'spaghetti noodles', 'chow mein noodles', 'macaroni noodles', 'bread crumbs', 'rice', 'tortillas', 'bread'].sort();
-let allSpices = ['turmeric', 'cumin', 'oregano', 'paprika', 'parsley', 'garlic powder', 'red chili flakes', 'yellow curry powder', 'cayenne pepper', 'chili powder', 'rosemary'].sort();
-let allOther = ['brown sugar', 'cashews', 'sugar', 'sesame oil', 'cornstarch', 'rice vinegar', 'capers', 'pine nuts', 'red curry paste', 'soy sauce', 'oyster sauce', 'balsamic vinegar', 'vinegar','ketchup', 'mustard', 'hoisin sauce', 'beef bouillon', 'stock', 'honey', 'enchilada sauce', 'flour', 'baking soda'].sort();
+let allProtein = ['(select all proteins)', 'chicken breast', 'chicken thighs', 'pork', 'beef brisket', 'flank steak', 'corned beef', 'ground beef', 'salmon', 'spam', 'egg', 'black beans'].sort();
+let allVegFruit = ['(select all veg/fruit)', 'garlic', 'ginger', 'onion', 'green onion', 'tomato', 'potato', 'green beans', 'peppers', 'mushrooms', 'carrot', 'peas', 'cabbage', 'seaweed', 'hominy', 'guajillo chile', 'cilantro', 'basil', 'lemon', 'lime', 'bok choy', 'broccoli', 'edamame', 'corn'].sort();
+let allDairy = ['(select all dairy)', 'feta cheese', 'parmesan cheese', 'cheddar cheese', 'cheese blend', 'milk', 'cream', 'butter', 'coconut milk', 'yogurt'].sort();
+let allCarbs = ['(select all carbs)', 'ramen noodles', 'spaghetti noodles', 'chow mein noodles', 'macaroni noodles', 'bread crumbs', 'rice', 'tortillas', 'bread'].sort();
+let allSpices = ['(select all spices)', 'turmeric', 'cumin', 'oregano', 'paprika', 'parsley', 'garlic powder', 'red chili flakes', 'yellow curry powder', 'cayenne pepper', 'chili powder', 'rosemary'].sort();
+let allOther = ['(select all other)', 'brown sugar', 'cashews', 'sugar', 'sesame oil', 'cornstarch', 'rice vinegar', 'capers', 'pine nuts', 'red curry paste', 'soy sauce', 'oyster sauce', 'balsamic vinegar', 'vinegar','ketchup', 'mustard', 'hoisin sauce', 'beef bouillon', 'stock', 'honey', 'enchilada sauce', 'flour', 'baking soda'].sort();
 
 let listOne = document.getElementById('proteins');
 let listTwo = document.getElementById('vegetables');
@@ -33,57 +33,50 @@ let listFour = document.getElementById('carbs');
 let listFive = document.getElementById('spices');
 let listSix = document.getElementById('other');
 
-let options = [allProtein, allVegFruit, allDairy, allCarbs, allSpices, allOther];
+let options = {
+  name: ['allProtein', 'allVegFruit', 'allDairy', 'allCarbs', 'allSpices', 'allOther'],
+  items: [allProtein, allVegFruit, allDairy, allCarbs, allSpices, allOther]
+}
 let allList = [listOne, listTwo, listThree, listFour, listFive, listSix];
 
-for (let i = 0; i < options.length; i++) {
-  for (let j = 0; j < options[i].length; j++) {
+for (let i = 0; i < options.items.length; i++) {
+  for (let j = 0; j < options.items[i].length; j++) {
     let li = document.createElement('li');
     let input = document.createElement('input');
     let label = document.createElement('label');
-    input.setAttribute('value', options[i][j]);
+    input.setAttribute('value', options.items[i][j]);
     input.type = 'checkbox';
-    // input.setAttribute('name', 'ckbx');
-    input.setAttribute('id', options[i][j]);
-    label.setAttribute('for', options[i][j]);
-    label.innerText = options[i][j];
+    input.setAttribute('name', options.name[i]);
+    input.setAttribute('id', options.items[i][j]);
+    if (j === 0){
+      input.setAttribute('class', '(select all)');
+    }
+    label.setAttribute('for', options.items[i][j]);
+    label.innerText = options.items[i][j];
     allList[i].appendChild(li);
     li.appendChild(input);
     li.appendChild(label);
   }
 }
 
+function selectAll() {
+  console.log('worked')
+  for(let i = 0; i < selectAllbtn.length; i++){
+    console.log(`${selectAllbtn[i].name}: `, selectAllbtn[i].checked);
+    if(selectAllbtn[i].checked === true){
+      console.log('name: ', options.name[i]);
+      for(let j = 0; j < options.items[i].length; j++){
+        let listItem = document.getElementsByName(options.name[i]);
+        listItem[j].checked = true;
+      }
+    }
+  }
+}
 
-
-// function selectAll() {
-//   console.log('worked')
-//   for(let i = 0; i < options.length; i++ ){
-//     if(options[i][0].checked === true){
-//       for(let j = 0; j < options[i].length; j++){
-//         console.log('hey!');
-//         cb[j].checked = true;
-//       }
-//     }
-//   }
-//   // let select = document.getElementsByName('ckbx');
-//   // console.log(select.length);
-//   // console.log(typeof select);
-//   // for (var checkbox in select)
-//   //   checkbox.checked = source.checked;
-//   // // for (let i = 0; i < select.length; i++) {
-//   // //   if (select[i].type == 'checkbox') {
-//   // //     select = true;
-//   // //   }
-//   // // }
-// }
-// // for(let i = 0; i < options.length; i++ ){
-// //   options[i][0].setAttribute('name', 'ckAll');
-// // }
-
-
-// let selectAllbtn = document.getElementById('(select all)');
-// selectAllbtn.addEventListener('click', selectAll);
-
+let selectAllbtn = document.getElementsByClassName('(select all)');
+for (let i = 0; i < selectAllbtn.length; i++){
+  selectAllbtn[i].addEventListener('click', selectAll);
+}
 
 let cb = document.querySelectorAll('input');
 let myIngredients = [];
